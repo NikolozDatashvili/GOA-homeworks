596c3e7c3fbcea2b8027474e2e5419f5a4333cee
NikadataPep <nikatg9@gmail.com>
2024-06-15 20:46:34 +0400

    10th commit

---
+++ level10/classwork/level10.py
@@ -0,0 +1,29 @@
+# password = "Nika123"
+
+# user_input = input("Enter the password: ")
+
+
+# if password == user_input:
+#     print("You succesfully authenticated")
+# else:                
+#     print("Password is invalid")
+#     print("Try again")
+
+
+
+# product = 1
+
+# for i in range(5):
+#     num = int(input("Please enter number: "))
+#     product = product * num
+
+# print(product)
+
+
+sum = 1
+
+for i in range(5, 16):
+    i = i + i
+   
+
+print(i)
---
+++ level10/homework/level10.py
@@ -0,0 +1,40 @@
+
+# sum = 0
+# multiplication = 1 
+# division = 1 
+# remainder = 1
+
+# for i in range(1,101):
+#     sum = sum + i 
+#     multiplication = multiplication * i
+#     division = division / i
+#     remainder = remainder % i
+
+
+    
+
+# print(str(sum)+ "\n" + str(multiplication) + "\n" +  str(division) + "\n" +  str(remainder))
+
+
+
+# user_input = int(input("Enter the number: "))
+
+# sum = 0
+# for i in range(user_input):
+#     sum = sum + i 
+
+# print(sum)
+
+
+num1 = int(input("Enter the number: "))
+
+num2 = int(input("Enter the number: "))
+
+sum = num1 + num2 
+
+multiply = num1 * num2 
+
+for i in range(1):
+    print(sum)
+    print(multiply)
+
