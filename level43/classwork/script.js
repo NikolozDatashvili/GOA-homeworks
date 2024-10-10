d385bd43329bf4b02a67de17f7b7c6f045a53986
NikadataPep <nikatg9@gmail.com>
2024-10-10 15:49:37 +0400

    js DOM

---
+++ level43/classwork/script.js
@@ -0,0 +1,11 @@
+let par1 = document.getElementById("p1");
+
+par1.textContent = "NEW TEXT";
+
+let h1 = document.getElementById("header1");
+
+h1.style.color = "red";
+
+let btn1 = document.getElementById("btn");
+
+btn1.style.color = "blue";
---
+++ level43/homework/script.js
@@ -0,0 +1,40 @@
+//// exercise N1
+let header1 = document.getElementById("h1");
+
+header1.textContent = "Training";
+header1.style.color = "#654321";
+
+/// exercise N2
+let header2 = document.getElementById("h2");
+
+header2.style.color = "teal";
+
+//\ exercise N3
+
+let div1 = document.getElementById("div1");
+
+div1.style.backgroundColor = "teal";
+div1.style.color = "cornsilk";
+div1.style.fontSize = "25px";
+
+//| exercise N4-N5
+
+let btn1 = document.getElementById("btn1");
+
+btn1.style.marginLeft = "1100px";
+btn1.style.color = "teal";
+btn1.style.backgroundColor = "#CFB595";
+btn1.style.padding = "20px";
+btn1.style.fontSize = "25px";
+btn1.style.border = "none";
+btn1.style.cursor = "pointer";
+
+let div2 = document.getElementById("div2");
+
+div2.style.backgroundColor = "#E0CDA9";
+
+let p2 = document.getElementById("p2");
+
+p2.style.color = "#412920";
+p2.style.fontSize = "30px";
+p2.style.marginLeft = "1125px";
