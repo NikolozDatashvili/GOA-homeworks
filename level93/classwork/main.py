71029f33e4378a0d4fb567ec90150723b460706f
NikadataPep <nikatg9@gmail.com>
2025-04-08 18:30:27 +0400

    map, filter...

---
+++ level93/classwork/main.py
@@ -0,0 +1,57 @@
+
+
+
+# func = lambda x: x % 2 
+
+# print(func(5))
+
+numbers = [1, 3, 4, 6, 7, 9, 10]
+
+# my_set = {12,54.5, "hi", True, 1}
+# print(my_set)
+
+
+# my_dict = {"name": "nika", "lastname": "data", "age": 17}
+
+# print(my_dict["name"])
+# print(my_dict["lastname"])
+# print(my_dict["age"])
+# print(my_dict)
+
+# double = list(map(lambda x: x * 2, numbers))
+# print(double)
+
+
+
+# res = list(filter(lambda x: x % 3 == 0, numbers))
+# print(res)
+
+
+
+# my_list = [12,44,564,4,16,57]
+
+# func = lambda x: x > 10
+
+# res =list( filter(func, my_list))
+# print(res)
+
+
+
+# add_ten = list(map(lambda x: x + 10, numbers))
+# numbers += add_ten
+# print(numbers)
+
+# only_even = lambda x: x % 2 != 0
+
+# res = list(filter(only_even, numbers))
+# print(res)
+
+
+# from functools import reduce
+# list_sum = lambda x, y: x + y
+# result = reduce(list_sum, numbers)
+# print(result)
+
+
+new_set = {"hi", 121, 67}
+print(new_set)
---
+++ level93/homework/main.py
@@ -0,0 +1,27 @@
+
+# #<> exrcise N1
+# #// Greeting user
+# user_input = input("Please enter your name: ")
+# func = lambda x: f"Hello {x}"
+# print(func(user_input))
+
+
+#<> exrcise N2
+#// Doubling numbers
+my_list = [12,43,5,1,55,64]
+
+my_func = lambda x: x * 2
+result =list( map(my_func, my_list))
+print(result)
+
+#<> exrcise N3
+#// multipes of 5 
+func1 = lambda x: x % 5 ==0
+
+result1 = list(filter(func1, my_list))
+
+print(result1)
+
+#<>exercise N4
+setn = {"Goa", "Novatori", "Stem", 123, True, 1, False, 0}
+#/ 0 და 1 არ გამოვა რადგან true და 1, false და 0 ტოლი მნიშვნელობებია
